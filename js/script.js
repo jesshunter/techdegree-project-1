6b35a200b5f7fbc831258e05b222cc220043fe15
jesshunter <JessHunter90@gmail.com>
2022-10-26 17:47:16 -0400

    Created quotes array of objects

--- js/script.js
+++ js/script.js
@@ -3,13 +3,57 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
+const quotes = [
+  {
+    quote: "Love as powerful as your mother’s for you leaves it’s own mark. To have been loved so deeply, even though the person who loved us is gone, will give us some protection forever.",
+    source: "Albus Dumbledore",
+    citation:"Harry Potter and the Sorcerer’s Stone",
+    year: 1997,
+    tags: ["love", "sacrifice"]
+  },
+  {
+    quote: "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.",
+    source: "Albus Dumbledore",
+    citation:"Harry Potter and the Sorcerer’s Stone",
+    year: 1997,
+    tags: ["courage", "wisdom"]
+  },
+  {
+    quote: "It is the unknown we fear when we look upon death and darkness, nothing more.",
+    source: "Albus Dumbledore",
+    citation:"Harry Potter and the Half-Blood Prince",
+    year: 2005,
+    tags: ["wisdom", "encouragement"]
+  },
+  {
+    quote: "I am what I am, an’ I’m not ashamed. ‘Never be ashamed,’ my ol’ dad used ter say, ‘there’s some who’ll hold it against you, but they’re not worth botherin’ with.",
+    source: "Rubeus Hagrid",
+    citation:"Harry Potter and the Goblet of Fire",
+    year: 2000,
+    tags: ["wisdom", "encouragement"]
+  },
+  {
+    quote: "If you want to know what a man’s like, take a good look at how he treats his inferiors, not his equals.",
+    source: "Sirius Black",
+    citation:"Harry Potter and the Goblet of Fire",
+    year: 2000,
+    tags: ["wisdom"]
+  },
+  {
+    quote: "Every human life is worth the same, and worth saving.",
+    source: "Kingsley Shacklebolt",
+    citation:"Harry Potter and the Deathly Hallows",
+    year: 2007,
+    tags: ["motivation", "wisdom"]
+  }
+];
 
 
 
